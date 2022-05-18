77bdfb6acec29e17197310bf2219dea930184cbc
Wilk Gillian <wilkrosa1998@gmail.com>
2022-05-18 00:43:14 -0400

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { ChakraProvider, Box, theme, Image, Flex } from '@chakra-ui/react';
+import { Albums } from './components/albums';
+import { Header } from './components/header';
+import { DataProvider } from './context';
+
+export const App = () => (
+  <DataProvider>
+    <ChakraProvider theme={theme}>
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        align="center"
+        position="fixed"
+        width="100%"
+        height="100vh"
+      >
+        <Image width="100%" height="100%" src="./assets/background.png" />
+      </Flex>
+      <Box
+        display="flex"
+        justifyContent="center"
+        position="relative"
+        width="100%"
+        height="100vh"
+      >
+        <Box
+          display="inline-block"
+          bg="rgba(250, 250, 250, 0.5)"
+          width="80%"
+          maxHeight="auto"
+          my={5}
+          position="absolute"
+        >
+          <Albums />
+        </Box>
+        <Header />
+      </Box>
+    </ChakraProvider>
+  </DataProvider>
+);
